327e40ecf3db6859a333bfac9be08680baad0c3f
Liz Tiller <elizabeth.tiller@gmail.com>
2017-01-26 12:47:41 -0500

    unbroke it

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,12 @@ import cx from 'classNames'
 
 class Card extends Component {
   static propTypes = {
-    isUp: React.PropTypes.bool,
     image: React.PropTypes.string,
     handleClick: React.PropTypes.func
   }
 
   render () {
-    return <div className={cx('card', {'up': true, 'down': false})}>
+    return <div className={cx('card', {up: true, down: false})}>
       {data.cards.map((card, i) => {
         return <div key={i}>
           <img src={card.image} />
