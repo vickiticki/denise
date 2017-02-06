14a07f31cce96f329b5565ff51a6dc11bd34bcd7
Liz Tiller <elizabeth.tiller@gmail.com>
2017-02-06 15:37:31 -0500

    fixed typos

--- src/components/History.js
+++ src/components/History.js
@@ -6,11 +6,11 @@ class History extends Component {
     return <div className='history'>
       <h1>History of Tarot</h1>
       <div className='historyP'>
-        <p>Playing cards were introduced in Europe in the mid-fourteenth century and consisted of four suits.  Soon, a set of triumph cards were added and eventually became known as trump cards.  Only the extremely wealthy could own this beautiful, hand painted cards and the Visconti-Sforza owned several famous decks. With the invention of the printing press, the cards became more affordable and the Tarot de Marseilles became a popular design and established the motifs of the triumph cards.</p>
+        <p>Playing cards were introduced in Europe in the mid-fourteenth century and consisted of four suits.  Soon, a set of triumph cards were added and eventually became known as trump cards.  Only the extremely wealthy could own these beautiful, hand painted cards and the Visconti-Sforza owned several famous decks. With the invention of the printing press, the cards became more affordable and the Tarot de Marseilles became a popular design and established the motifs of the triumph cards.</p>
 
         <p>Beginning in the mid sixteenth century, tarot cards were sometimes used for divination.  Modern tarot decks consist of 78 cards.  The Major Arcana consist of 22 cards numbered from 0 to 21. These cards tell the story of a metaphysical journey beginning with card 0, The Fool—a young man with no experience of the world and ends with card 21—The World. The four suits are called the Minor Arcana and named wands, swords, cups, and pentacles. The suits have cards numbered from 1-10 and 4 court cards, the King, Queen, Knight, and Page/Jack.</p>
 
-        <p>The Rider-Waite-Smith deck was first published in 1910 and the design is widely known.  Its popularity codified the symbolism of the Major Arcana. Recently, self-publishing has become popular and has encouraged artists to create a variety of different styles. Today's practitioners can find decks the more closely reflect their personal esthetics
+        <p>The Rider-Waite-Smith deck was first published in 1910 and the design is widely known.  Its popularity codified the symbolism of the Major Arcana. Recently, self-publishing has become popular and has encouraged artists to create a variety of different styles. Today's practitioners can find decks that more closely reflect their personal esthetics
         </p>
       </div>
       <footer>
